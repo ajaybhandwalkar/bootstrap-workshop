d95bff0a05675270e5dd7c6613ef3a8409c30823
Ajay <ajaybhan@cybage.com>
2022-02-03 17:57:56 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function getInfo() {
+    var data = document.getElementById('data').value;
+    if(data=="Celery root"){
+        window.alert(data+" is in stock");
+    }
+    else if(data=="Spaghetti Splash") {
+        window.alert(data+" is in stock");
+    }
+    else if(data=="Killer mushroom") {
+        window.alert(data+" is in stock");
+    }
+    else
+        window.alert(data+" is not Available");
+
+
+}
